cd8151bcaa6cc22900658aad9ab14897488bc5af
Nisarg Patel <patel321nisarg@gmail.com>
2021-10-24 22:01:24 -0400

    a6 React fa21

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
 import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
 
@@ -9,22 +7,24 @@ import Practice from "./components/a6/Practice";
 import {BrowserRouter, Route} from "react-router-dom";
 import ExploreScreen from "./components/a6/Build/ExploreScreen";
 import HomeScreen from "./components/a6/Build/HomeScreen";
+import A6 from "./components/a6";
 
 function App() {
   return (
       <BrowserRouter>
           <div className="container">
-              <Route path="/a6/hello" exact={true}>
+              <Route path={["/", "/a6", "/a6/hello"]} exact={true}>
                   <HelloWorld/>
               </Route>
-              <Route path={["/", "/a6", "/a6/practice"]} exact={true}>
+              <Route path="/a6/practice" exact={true}>
+                  <A6/>
                   <Practice/>
               </Route>
-              <Route path={"/a6/twitter/home"} exact={true}>
-                  <HomeScreen/>
-              </Route>
-              <Route path={"/a6/twitter/explore"} exact={true}>
+              <Route path="/a6/twitter/explore" exact={true}>
                   <ExploreScreen/>
+              </Route>
+              <Route path="/a6/twitter/home" exact={true}>
+                  <HomeScreen/>
               </Route>
           </div>
       </BrowserRouter>
