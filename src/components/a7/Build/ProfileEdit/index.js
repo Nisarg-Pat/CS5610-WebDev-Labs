ceb3deb58d7bb406f882d42c3d9da438a6501fc0
Nisarg Patel <patel321nisarg@gmail.com>
2021-11-11 16:26:39 -0500

    a7 Redux fa21

--- src/components/a7/Build/ProfileEdit/index.js
+++ src/components/a7/Build/ProfileEdit/index.js
@@ -1,9 +1,59 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import {useSelector} from "react-redux";
+import React, {useState} from "react";
+import {useHistory} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
 
 const ProfileEdit = () => {
     const profile = useSelector((state) => state.profile)[0];
+    const [profileState, setProfile] = useState({...profile});
+    console.log(profileState);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const profileChangeHandler = (change, inputType) => {
+        let newProfile = profileState;
+        switch (inputType) {
+            case "nameChange":
+                newProfile = {
+                    ...profile,
+                    name:change
+                }
+                setProfile(newProfile);
+                break;
+            case "bioChange":
+                newProfile = {
+                    ...profile,
+                    bio:change
+                }
+                setProfile(newProfile);
+                break;
+            case "locationChange":
+                newProfile = {
+                    ...profile,
+                    location:change
+                }
+                setProfile(newProfile);
+                break;
+            case "websiteChange":
+                newProfile = {
+                    ...profile,
+                    website:change
+                }
+                setProfile(newProfile);
+                break;
+            default:
+                break;
+        }
+    }
+
+    const saveClickHandler = () => {
+        const action = {
+            type: "profileChange",
+            profile: profileState
+        }
+        dispatch(action);
+        history.push("/a7/twitter/profile");
+    }
+
     return(
         <div className="row pt-2">
             <div className="col-1 wd-flex wd-v-center pb-2">
@@ -15,7 +65,8 @@ const ProfileEdit = () => {
                 </div>
             </div>
             <div className="col-2 pb-2">
-                <button className="btn btn-primary p-2 wd-btn wd-tweet-btn">
+                <button className="btn btn-primary p-2 wd-btn wd-tweet-btn"
+                        onClick={saveClickHandler}>
                     Save
                 </button>
             </div>
@@ -32,15 +83,22 @@ const ProfileEdit = () => {
             <div className="col-12 pt-3">
                 <label className="wd-editProfileInput-div">
                     <span className="wd-color-grey">Name</span><br/>
-                    <input className="wd-editProfileInput" value={profile.name}/>
+                    <input onChange={(event) => {
+                        profileChangeHandler(event.target.value, "nameChange")
+                    }}
+                           className="wd-editProfileInput"
+                           value={profileState.name}/>
                 </label>
             </div>
 
             <div className="col-12 pt-3">
                 <label className="wd-editProfileInput-div">
                     <span className="wd-color-grey">Bio</span><br/>
-                    <textarea className="wd-editProfileInput" value={profile.bio}>
-
+                    <textarea className="wd-editProfileInput"
+                              value={profileState.bio}
+                              onChange={(event) => {
+                                  profileChangeHandler(event.target.value, "bioChange")
+                              }}>
                     </textarea>
                 </label>
             </div>
@@ -48,14 +106,23 @@ const ProfileEdit = () => {
             <div className="col-12 pt-3">
                 <label className="wd-editProfileInput-div">
                     <span className="wd-color-grey">Location</span><br/>
-                    <input className="wd-editProfileInput" value={profile.location}/>
+                    <input className="wd-editProfileInput"
+                           value={profileState.location}
+                           onChange={(event) => {
+                               profileChangeHandler(event.target.value, "locationChange")
+                           }}/>
                 </label>
             </div>
 
             <div className="col-12 pt-3">
                 <label className="wd-editProfileInput-div">
                     <span className="wd-color-grey">Website</span><br/>
-                    <input className="wd-editProfileInput" value={profile.website} placeholder="Website"/>
+                    <input className="wd-editProfileInput"
+                           value={profileState.website}
+                           placeholder="Website"
+                           onChange={(event) => {
+                               profileChangeHandler(event.target.value, "websiteChange")
+                           }}/>
                 </label>
             </div>
         </div>
