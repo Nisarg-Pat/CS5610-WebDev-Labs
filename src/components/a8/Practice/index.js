fbf822458ebf1f5559ff2327ffef17984cba6338
Nisarg Patel <patel321nisarg@gmail.com>
2021-11-20 11:24:48 -0500

    A8 Practice

--- src/components/a8/Practice/index.js
+++ src/components/a8/Practice/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReduxExamples from "./ReduxExamples/components";
+import APIExamples from "./APIExamples";
 
 const Practice = () => {
     return(
         <div>
             <h1>Practice</h1>
+            <APIExamples/>
             <ReduxExamples/>
         </div>
     );
