a99ac56020fb8ff130f07969e95808282defd5ce
Nisarg Patel <patel321nisarg@gmail.com>
2021-11-25 16:28:16 -0500

    Practice a9

--- src/a9/Practice/index.js
+++ src/a9/Practice/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReduxExamples from "./ReduxExamples/components";
 import APIExamples from "./APIExamples";
+import Movies from "./Movies";
 
 const Practice = () => {
     return(
         <div>
             <h1>Practice</h1>
+            <Movies/>
             <APIExamples/>
             <ReduxExamples/>
         </div>
